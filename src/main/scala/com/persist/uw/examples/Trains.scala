c456a7cfc69a3fbc9fa738b55aa03f071c8e43b7
Chattopadyay, Dipayan (DS) <Dipayan.Chattopadyay@cdk.com>
2016-11-25 12:05:10 -0800

    Updating reading file to connectionList. Work in progress

--- src/main/scala/com/persist/uw/examples/Trains.scala
+++ src/main/scala/com/persist/uw/examples/Trains.scala
@@ -1,6 +1,10 @@
 package com.persist.uw.examples
 
+import java.util.concurrent
+import java.util.concurrent.TimeUnit
+
 import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
 import scala.io.Source
 import scala.concurrent.duration._
 
@@ -20,6 +24,19 @@ case class Info(path: Option[Path])
 
 class Trains {
 
+  def readFile() : List[Connection]  = {
+    val connections = new ListBuffer[Connection]
+    val lines =  scala.io.Source.fromFile("data/trains.txt").getLines()
+    while (lines.hasNext){
+      val conns = lines.next().split(",")
+      val timeSplits = conns(2).split(":")
+      val time: FiniteDuration  = Some( Duration(timeSplits(0).toInt,TimeUnit.HOURS)  +  Duration(timeSplits(1).toInt,TimeUnit.MINUTES) ).collect { case d: FiniteDuration => d}.get
+      connections += Connection(Station(conns(0)),Station(conns(1)),time)
+    }
+    connections.toList
+  }
+
+
   def toParis(): Seq[(Station, Info)] = ???
 }
 
@@ -27,6 +44,7 @@ object Trains {
 
   def main(args: Array[String]): Unit = {
     val t = new Trains
+    t.readFile().foreach(print)
     val state = t.toParis()
     println("")
     for ((station, info) <- state) {
