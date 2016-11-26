d70bf9305691e26954cec7b1a5da872d104c2cee
dipayan90 <dipayan90@gmail.com>
2016-11-25 19:52:36 -0800

    Some more progress . The 2d array is mostly created

--- src/main/scala/com/persist/uw/examples/Trains.scala
+++ src/main/scala/com/persist/uw/examples/Trains.scala
@@ -38,7 +38,7 @@ class Trains {
   }
 
   def getDuration(start : Station , destination: Station) : Duration = {
-    val conns = connections.filter(e => e.from.equals(start) && e.to.equals(destination))
+    val conns = connections.filter(e => ( e.from.equals(start) && e.to.equals(destination)) || (e.from.equals(destination) && e.to.equals(start)))
     if(conns.nonEmpty){
       conns.head.time
     }else{
@@ -46,31 +46,71 @@ class Trains {
     }
   }
 
-  def findMinimumFromMap(trackerMap: Map[(Station,Station),(Duration,Station)]) : Station = {
-    val res = trackerMap.toSeq.min(Ordering.by[((Station,Station),(Duration,Station)),Duration](_._2._1))
+  def findMinimumFromMap(trackerMap: Map[(Station,Station),(Duration,Station)], from: Station) : Station = {
+    val res = trackerMap.toSeq.filter(_._1._1.equals(from)).min(Ordering.by[((Station,Station),(Duration,Station)),Duration](_._2._1))
     res._1._2
   }
 
+  def toCover(stationsAlreadyCovered : List[Station]) : List[Station] = {
+    def contains(station: Station,stationList : List[Station]) : Boolean = {
+      for(stn <- stationList) {
+        if(stn.equals(station)){
+           return true
+        }
+      }
+      false
+    }
+    stations.filter(e => !contains(e,stationsAlreadyCovered))
+  }
+
+  def findColumnarMinimum(to : Station,trackerMap: Map[(Station,Station),(Duration,Station)]) : (Duration, Station) = {
+    if(trackerMap.isEmpty || !trackerMap.toSeq.exists(_._1._2.equals(to))){
+      (Duration.Inf,to)
+    }else{
+      val minimumStation : ((Station, Station), (Duration, Station)) = trackerMap.toSeq.filter(_._1._2.equals(to)).min(Ordering.by[((Station,Station),(Duration,Station)),Duration](_._2._1))
+      minimumStation._2
+    }
+  }
+
   val connections : List[Connection] = readFile()
-  val stations:  List[Station] = connections.map(_.from).distinct
+  val stations:  List[Station] = ( connections.map(_.from).distinct ::: connections.map(_.to).distinct).distinct
 
   // start with paris and then create a matrix with the values
   // map of (start Station,end Station) - > (Weight,Via Station)
   def implementDjktrasAlgorithm() = {
     val baseStation = Station("Paris")
-    val stationsToCover = stations.filter(!_.equals(baseStation))
+    var stationsAlreadyVisited = List.empty[Station]
+    // initially add the base station to the list of stations already visited
+    stationsAlreadyVisited =  baseStation :: stationsAlreadyVisited
 
+    // main tracker to track the shortest paths
     var trackerMap = Map.empty[(Station,Station),(Duration,Station)]
 
-    var startStation = baseStation
-    for(stn <- stationsToCover){
-      trackerMap = trackerMap + ((startStation,stn) -> (getDuration(startStation,stn),startStation))
+    while(stations.length > stationsAlreadyVisited.length){
+      for(stn <- toCover(stationsAlreadyVisited)){
+        val duration = getDuration(stationsAlreadyVisited.head,stn)
+        val columnMin = findColumnarMinimum(stn,trackerMap)
+        if(columnMin._1 < duration){
+          trackerMap = trackerMap + ((stationsAlreadyVisited.head,stn) -> columnMin)
+        }else{
+          trackerMap = trackerMap + ((stationsAlreadyVisited.head,stn) -> (duration,stationsAlreadyVisited.head))
+        }
+      }
+      System.out.println("################")
+      // print from axis
+      System.out.println("Currently visiting :  "+stationsAlreadyVisited.head.name)
+      System.out.println("**************Connections in this level *********")
+      // print map now for the from axis
+      trackerMap.filter(_._1._1.equals(stationsAlreadyVisited.head)).foreach(print)
+      //add the next min to the from axis
+      stationsAlreadyVisited = findMinimumFromMap(trackerMap,stationsAlreadyVisited.head) :: stationsAlreadyVisited
+      //next minimum is
+      System.out.println(" \n Next minimum is : "+ stationsAlreadyVisited.head.name)
     }
-    System.out.println("tracker map is, in 1st pass ")
-    System.out.println(trackerMap)
-    val nextMinimun = findMinimumFromMap(trackerMap)
-    System.out.println("next min is: ")
-    System.out.println(nextMinimun)
+
+
+    // Print tracker map here and see the output
+
   }
 
   def toParis(): Seq[(Station, Info)] = ???
